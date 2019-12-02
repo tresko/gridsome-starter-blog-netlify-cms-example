4ccd0aadbd1f4d05f47e7f914bb3a4a133698a4f
Miha Sedej <sedej.miha@gmail.com>
2019-12-02 18:22:19 +0100

    Add site_id

--- src/admin/index.js
+++ src/admin/index.js
@@ -1,1 +1,7 @@
 import CMS from 'netlify-cms'
+
+if (document.location.host.split(':')[0] !== 'localhost') {
+    CMS.configure({'site_id': 'cms.netlify.com' });
+} else {
+    CMS.configure({'site_id': 'gridsome-netflify-cms.netlify.com' });
+}
